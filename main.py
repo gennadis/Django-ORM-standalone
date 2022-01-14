8b665af261a649293de66d19857b4298ed509a49
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 21:30:23 +0300

    Print all and active passcards count

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Pass
 import os
 
 import django
@@ -8,12 +9,7 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == "__main__":
-    test_passcard = Passcard.objects.all()[0]
-    test_passcard_info = {
-        "owner_name": test_passcard.owner_name,
-        "passcode": test_passcard.passcode,
-        "created_at": test_passcard.created_at,
-        "is_active": test_passcard.is_active,
-    }
-
-    print(test_passcard_info)
+    all_passcards = Passcard.objects.all()
+    active_passcards = [passcard for passcard in all_passcards if passcard.is_active]
+    print(f"Всего пропусков {len(all_passcards)}")
+    print(f"Активных пропусков {len(active_passcards)}")
