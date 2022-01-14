f9f4e2381fa5f3af15dafa69c21054207a59ce31
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 22:20:22 +0300

    Add non-closed visits

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,19 +1,20 @@
-from datacenter.models import Passcard
 from datacenter.models import Visit
 from django.shortcuts import render
 
 
 def storage_information_view(request):
-    # Программируем здесь
+    active_visits = Visit.objects.filter(leaved_at=None)
+    non_closed_visits = []
 
-    non_closed_visits = [
-        {
-            'who_entered': 'Richard Shaw',
-            'entered_at': '11-04-2018 25:34',
-            'duration': '25:03',
+    for visit in active_visits:
+        visit_info = {
+            "who_entered": visit.passcard.owner_name,
+            "entered_at": visit.entered_at,
+            "duration": visit.format_duration(visit.get_duration()),
         }
-    ]
+        non_closed_visits.append(visit_info)
+
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        "non_closed_visits": non_closed_visits,  # не закрытые посещения
     }
-    return render(request, 'storage_information.html', context)
+    return render(request, "storage_information.html", context)
