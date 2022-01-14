cf676b32dc56b522a59b0b1b4f0e6ae1ec72200c
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 21:35:10 +0300

    Use production ready template

--- main.py
+++ main.py
@@ -1,15 +1,6 @@
-from ast import Pass
 import os
 
-import django
+from django.core.management import execute_from_command_line
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
-django.setup()
-
-from datacenter.models import Passcard  # noqa: E402
-
-if __name__ == "__main__":
-    all_passcards = Passcard.objects.all()
-    active_passcards = Passcard.objects.filter(is_active=True)
-    print(f"Всего пропусков {len(all_passcards)}")
-    print(f"Активных пропусков {len(active_passcards)}")
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+execute_from_command_line('manage.py runserver 0.0.0.0:8000'.split())
