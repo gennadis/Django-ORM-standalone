eaeb73bf5d5433b021d567dc4d2169baf7404529
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 22:37:27 +0300

    Refactor for naming consistency

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -7,12 +7,12 @@ def storage_information_view(request):
     non_closed_visits = []
 
     for visit in active_visits:
-        visit_info = {
+        visit_details = {
             "who_entered": visit.passcard.owner_name,
             "entered_at": visit.entered_at,
             "duration": visit.format_duration(visit.get_duration()),
         }
-        non_closed_visits.append(visit_info)
+        non_closed_visits.append(visit_details)
 
     context = {
         "non_closed_visits": non_closed_visits,  # не закрытые посещения
