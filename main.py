81e02d50d8a4f98ae414ac6f44d21046ba624493
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 21:21:05 +0300

    Print passcards count

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import os
 
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 django.setup()
 
 from datacenter.models import Passcard  # noqa: E402
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print("Количество пропусков:", Passcard.objects.count())  # noqa: T001
