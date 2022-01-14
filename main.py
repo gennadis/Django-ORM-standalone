056288336356bc67be346e9080a6ea9eb08353bd
Gennady Badinov <gennady.badinov@gmail.com>
2022-01-14 21:31:41 +0300

    Refactor active passcards with ORM filtering

--- main.py
+++ main.py
@@ -10,6 +10,6 @@ from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == "__main__":
     all_passcards = Passcard.objects.all()
-    active_passcards = [passcard for passcard in all_passcards if passcard.is_active]
+    active_passcards = Passcard.objects.filter(is_active=True)
     print(f"Всего пропусков {len(all_passcards)}")
     print(f"Активных пропусков {len(active_passcards)}")
